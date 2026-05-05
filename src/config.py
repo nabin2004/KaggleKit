8e1ffcbb34a7695397c13829830ec46fab625228
SwissKit Autobot <autobot@swisskit.com>
2026-05-05 08:17:15 +0545

    Update src/config.py for new constants

--- src/config.py
+++ src/config.py
@@ -1,2 +1,7 @@
-TRAINING_FILE = "../input/mnist_train_folds.csv"
+TRAINING_FILE = "../input/train_folds.csv"
 MODEL_OUTPUT = "../models/"
+
+TARGET_COLUMN = "PitNextLap"
+KFOLD_COLUMN = "kfold"
+METADATA_COLUMNS: tuple[str, ...] = ("kfold",)
+DROP_FROM_FEATURES: tuple[str, ...] = ()
